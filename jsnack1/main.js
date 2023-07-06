6cad437e54460d5df7482e927f7cbef0d5522c30
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-06 16:17:54 +0200

    jsnack 1 js random-complete-name found

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -8,11 +8,25 @@ con nome e cognome.
 */
 
 // creo una lista di nomi con un array 
-const nameList = ["marco","vanessa" ,"mario", "samuele", "luca", "paolo", "lino"];
+const nameList = ["marco", "vanessa", "mario", "samuele", "luca", "paolo", "lino"];
 
 // creo una lista di cognomi con un array 
-const lastNameList = ["rossi","verdi" ,"bianchi", "banfi", "ferrari", "moretti", "greco"];
+const lastNameList = ["rossi", "verdi", "bianchi", "banfi", "ferrari", "moretti", "greco"];
 
 // lista vuota dove dovrebbero essere creati i nomi casuali dati dai nomi e cognomi
 const randomNameList = [];
+
+// creo una costante che riesca a prendere un nome casuale dall'array "nameList"
+const randomNameI = Math.floor(Math.random() * nameList.length);
+const randomName = nameList[randomNameI];
+
+// creo una costante che riesca a prendere un nome casuale dall'array "lastNameList"
+const randomlastNameI = Math.floor(Math.random() * lastNameList.length);
+const randomLastName = lastNameList[randomlastNameI];
+
+let randomCompleteName = `${randomName} ${randomLastName}`;
+
+console.log(randomCompleteName);
+
+
 
