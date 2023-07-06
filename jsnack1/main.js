458643aa8f0167a37ed7314811dc7bcdf6a99394
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-06 15:00:00 +0200

    file creation and jsnack 1 start

---
+++ jsnack1/main.js
@@ -0,0 +1,18 @@
+
+/* 
+Snack1
+Generatore di “nomi cognomi” casuali:
+il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+e da queste vuole generare una falsa lista di invitati
+con nome e cognome. 
+*/
+
+// creo una lista di nomi con un array 
+const nameList = ["marco","vanessa" ,"mario", "samuele", "luca", "paolo", "lino"];
+
+// creo una lista di cognomi con un array 
+const lastNameList = ["rossi","verdi" ,"bianchi", "banfi", "ferrari", "moretti", "greco"];
+
+// lista vuota dove dovrebbero essere creati i nomi casuali dati dai nomi e cognomi
+const randomNameList = [];
+
