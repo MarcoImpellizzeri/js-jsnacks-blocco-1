0a567e37bb3ca0669d393c9a67db51884cdd91a2
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-06 19:23:10 +0200

    jsnack 2 dosent completed

---
+++ jsnack 2/main.js
@@ -0,0 +1,13 @@
+/*
+    Snack2 Crea un array di numeri interi
+    e fai la somma di tutti gli elementi che sono in posizione dispari. 
+*/
+
+// const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+// for (let i = 0; i < numbers.lenght; i++) {
+//     let singleNumber = numbers[i];
+//     numbers.indexOf(10);
+
+//     console.log();
+// }
