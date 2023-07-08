6db83be214e49ddbc35cf6afd50daeb0576a294b
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-08 12:24:56 +0200

    jsnack 2 done

--- jsnack 2/main.js
+++ jsnack 2/main.js
@@ -11,3 +11,15 @@
 
 //     console.log();
 // }
+
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+let sommaDispari = 0; 
+
+for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 !== 0) {
+        sommaDispari += numbers[i];
+    }
+}
+
+console.log(sommaDispari);
